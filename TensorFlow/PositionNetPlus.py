7af258b6c09381df8efa59608612eef8aa5164b6
Mostafa /payami <mostafa.payami@queensu.ca>
2025-01-19 15:49:47 -0500

    Update PositionNetPlus.py

--- TensorFlow/PositionNetPlus.py
+++ TensorFlow/PositionNetPlus.py
@@ -6,27 +6,31 @@ import numpy as np
 import scipy.io as sio
 import matplotlib.pyplot as plt
 
-Nt = 16            # Number of transmit antennas
+Nt = 32            # Number of transmit antennas
 Nfft = 1024        # FFT size of base modem
-M = 600            # OTFS frame length (delay)
-N = 10             # OTFS frame length (Doppler)
+M  = 600           # OTFS frame length (delay)
+N  = 10            # OTFS frame length (Doppler)
 eta = 0.15         # Pilot overhead
+K  = 6             # Sparsity
 Mt = 96            # Number of pilots along delay dimension
 Nv = 10            # Number of pilots along Doppler dimension
 Nc = 32            # Number of filters (feature maps)
-alpha = 0.8        # Binary Cross-Entropy weight
+Nf = 32            # Number of filters (feature maps)
+kappa = 0.8        # Binary Cross-Entropy weight
+# Binary Cross-Entropy Weights based on Probabilities of Occurrence of Nonzero Indices in 3GPP Spatial Channel Model
+# kappa = tf.constant(tf.concat([[0.5], 0.254 * tf.math.pow(0.952, tf.range(1, Mt, dtype=tf.float32))], axis=0))
 
-### Loading Datasets ###
+### Load Dataset ###
 
-yDD_train = sio.loadmat('yDD_Nt64_0p20.mat', mat_dtype=True)['yDD']       # Delay-Doppler domain Observation Vector
-Position_train = sio.loadmat('Position_Nt64_0p20.mat', mat_dtype=True)['Position']  # Delay-Doppler domain Support
+yDD_train = sio.loadmat('yDD_observation_vector_Nt32_0p15.mat')['yDD']           # Delay-Doppler domain Observation Vector
+Position_train = sio.loadmat('Position_Nt32_0p15.mat', mat_dtype=True)['Position']    # Delay domain Support: Position = sign(|H_ADD|)
 
 ### Learning Rate Schedule ###
 
 def learning_rate_scheduler(epoch):
-    if epoch < 12:
+    if epoch < 16:
         return 1e-3
-    elif epoch < 14:
+    elif epoch < 18:
         return 1e-4
     else:
         return 1e-5
@@ -36,16 +40,18 @@ learning_rate_schedule = tf.keras.callbacks.LearningRateScheduler(learning_rate_
 ### TensorFlow/Keras Custom Functions and Layers ###
 
 def Weighted_Binary_CrossEntropy(y_true, y_pred):
-    y_pred = tf.clip_by_value(y_pred, 1e-7, 1 - 1e-7)
-    WBCE   = -tf.math.reduce_mean(alpha * y_true * tf.math.log(y_pred) + (1.0 - alpha) * (1 - y_true) * tf.math.log(1 - y_pred))
+    y_pred = tf.clip_by_value(y_pred, 1e-6, 1 - 1e-6)
+    # WBCE   = (1.0 / kappa - 1.0) * y_true * tf.math.log(y_pred) + 1.0 * (1.0 - y_true) * tf.math.log(1.0 - y_pred)
+    WBCE   = kappa * y_true * tf.math.log(y_pred) + (1.0 - kappa) * (1.0 - y_true) * tf.math.log(1.0 - y_pred)
+    WBCE   = -tf.math.reduce_mean(WBCE)
     return WBCE
 
 class Permute_and_Negate_Layer(tf.keras.layers.Layer):
     def __init__(self, **kwargs):
         super(Permute_and_Negate_Layer, self).__init__(**kwargs)
     def call(self, inputs):
-        permuted = tf.reverse(inputs, axis=[1])
-        y = tf.concat([permuted[:, 0:1, :] * -1, permuted[:, 1:, :]], axis=1)
+        p = tf.reverse(inputs, axis=[1])
+        y = tf.concat([p[:, 0:1, :] * -1, p[:, 1:, :]], axis=1)
         return y
 
 ### PositionNet+ ###
@@ -85,12 +91,13 @@ yDDi = tf.keras.layers.Dense(1)(yDDi)
 yDDi = tf.keras.layers.Reshape((2, Mt*Nv))(yDDi)
 
 # Feature Extraction (Support Recovery)
-F11 = tf.keras.layers.Dense(Mt*Nv*Nt)(yDDr)           # Complex Dense layer
-F11 = tf.keras.layers.Dropout(0.2)(F11)
+F11 = tf.keras.layers.Dense(Mt*Nv*Nt)(yDDr)    # Complex Dense layer
+F11 = tf.keras.layers.Dropout(0.25)(F11)
 F21 = tf.keras.layers.Dense(Mt*Nv*Nt)(yDDi)
-F21 = tf.keras.layers.Dropout(0.2)(F21)
+F21 = tf.keras.layers.Dropout(0.25)(F21)
 F1  = tf.keras.layers.Add()([F11, F21])
-Feature = tf.keras.layers.Lambda(lambda x: x ** 2, output_shape=(2, Mt*Nv*Nt))(F1)     # Absolute Value layer (Squared)
+# Feature = tf.keras.ops.abs(F1)          # Absolute Value layer
+Feature = tf.keras.ops.square(F1)       # Squared Absolute Value layer
 Feature = tf.keras.layers.AveragePooling1D(pool_size=(2))(Feature)
 Feature = tf.keras.layers.Reshape((Mt, Nv, Nt, 1))(Feature)
 Feature = tf.keras.layers.Conv3D(Nc, (3, 3, 3), padding='same')(Feature)
@@ -98,9 +105,10 @@ Feature = tf.keras.layers.Conv3D(Nc, (3, 3, 3), padding='same')(Feature)
 Feature = tf.keras.layers.Conv3D(Nc, (3, 3, 3), padding='same')(Feature)
 Feature = tf.keras.layers.LayerNormalization()(Feature)
 Feature = tf.keras.layers.BatchNormalization(axis=1)(Feature)
-Feature = tf.keras.layers.Rescaling(2.5, offset=0)(Feature) 
+Feature = tf.keras.layers.Rescaling(2.5, offset=0.0)(Feature)
 
 Position = tf.keras.layers.Softmax(axis=1)(Feature)    # Softmax layer
+
 Position = tf.keras.layers.Dense(Nc)(Position)
 Position = tf.keras.layers.Dense(1)(Position)
 Position = tf.keras.layers.Reshape((Mt, Nv, Nt))(Position)
@@ -117,9 +125,9 @@ PositionNetPlus.summary()
 
 PositionNetPlus.compile(optimizer=tf.keras.optimizers.AdamW(),
               loss=[tf.keras.losses.CosineSimilarity()],        # Weighted_Binary_CrossEntropy
-              metrics=['mse'])
+              metrics=['mse', 'binary_crossentropy'])           # 'cosine_similarity'
 
-PN_history = PositionNetPlus.fit(yDD_train, Position_train, validation_split=0.20, epochs=15, callbacks=[learning_rate_schedule])
+PN_history = PositionNetPlus.fit(yDD_train, Position_train, validation_split=0.25, epochs=20, callbacks=[learning_rate_schedule])
 
 PositionNetPlus.save("PositionNetPlus.keras")
 
@@ -132,4 +140,3 @@ plt.legend(['Train', 'Test'], loc='upper right')
 plt.grid(True, linestyle='--', alpha=0.75)
 plt.tight_layout()
 plt.show()
-
